5aff18ae27eee5c2910a5a0a14d201bd12c92e13
Varun Verma <28varun28pp@gmail.com>
2022-03-20 11:05:39 +0530

    done navbar-routes & footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import "./App.css";
+import {Routes, Route} from "react-router-dom";
+import {Home, Product, Login, Signup, Cart, Wishlist} from "./pages/pages";
+import {Navbar, Footer} from "./components/Components.jsx";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col min-h-screen">
+      <div className='header'>
+        <Navbar /> 
+      </div>
+      <div className='main grow'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/product' element={<Product />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/signup' element={<Signup />}/>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/wishlist' element={<Wishlist />}/>
+        </Routes> 
+      </div>
+      <div className='footer'>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Logo from "../assets/logo_svg.svg";
+import {Link} from "react-router-dom";
+import "../stylesheets/navbar.css";
+
+export function Navbar() {
+  return (
+    <header className="shoebox-navbar">
+        <nav>
+            <div className="shoebox-logo">
+                <Link to="/"><img src={Logo} alt="shoebox-logo" /></Link>
+            </div>
+            <div className="search-bar">
+                <div className="input-group">
+                    <input className="simple-input" id="name-input" type="text" placeholder="Search" />
+                </div>
+            </div>
+            <ul className="nav-links">
+                <li>
+                    <Link to="/login"><button className="btn btn-outline">Sign-in</button></Link>
+                </li>
+                <li>
+                    <Link to="/wishlist">
+                        <div className="badge-container">
+                            <i className="shopping-cart fa-regular fa-heart">
+                                <span className="badge badge-ico">0</span>
+                            </i>
+                        </div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/cart">
+                        <div className="badge-container">
+                            <i className="shopping-cart fa-solid fa-cart-shopping">
+                                <span className="badge badge-ico">0</span>
+                            </i>
+                        </div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
