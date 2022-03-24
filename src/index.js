46c1959ac0dc0956258513dbec95d3a30e5f73a0
Varun Verma <28varun28pp@gmail.com>
2022-03-24 22:30:09 +0530

    add wishlist functionality

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import {BrowserRouter} from "react-router-dom";
 import { makeServer } from "./server";
 import {ProductProvider} from "./utilities/product-context";
+import {WhishlistProvider} from "./utilities/wishlist-context";
 
 // Call make Server
 makeServer();
@@ -13,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProductProvider>
-        <App />
+        <WhishlistProvider>
+          <App />
+        </WhishlistProvider>
       </ProductProvider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React from 'react'
+import { Empty } from '../../components/Empty';
+import {useWishlist} from "../../utilities/wishlist-context";
+import "./wishlist.css";
 
 function Wishlist() {
+  const {wishlistState, wishlistDispatch} = useWishlist();
   return (
-    <div>Wishlist</div>
+    <div className='wishlist-container'>
+    <h3>Wishlist</h3>
+    <div  className='flex flex-wrap gap-4 m-4 justify-evenly'>
+    {
+      wishlistState.quantity === 0 
+      ? <Empty emptyText="Wishlist" />
+      : wishlistState.items.map(item => 
+            <div key={item._id} className="card card-vertical dismiss-card">
+              <div className="card-head">
+                  <div className="card-img">
+                      <img src={item.prodImage} alt="wishlist" />
+                  </div>
+                  <div className="card-texts">
+                      <h4 className="card-title">{item.brand}</h4>
+                      <p className="text-base">{item.title}</p>
+                      <p className="card-text"><span className="price-now text-md">
+                      Rs. {item.price}</span></p>
+                  </div>
+              </div>
+              <div className="card-buttons">
+                  <button className="card-btn-primary">Add to cart</button>
+              </div>
+              <button className="card-exit-btn"
+                onClick={() => wishlistDispatch({type:"REMOVE-FROM-WISHLIST", 
+                payload: item._id})}>
+              <i className="fas fa-times"></i></button>
+            </div>
+    )}
+    </div>
+    </div>
   )
 }
 
