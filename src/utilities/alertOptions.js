4177044bd211b2c0e91e4e6973e6f9b2406c0c7b
Varun Verma <28varun28pp@gmail.com>
2022-08-05 15:40:11 +0530

    update: alert timer

--- src/utilities/alertOptions.js
+++ src/utilities/alertOptions.js
@@ -2,7 +2,7 @@ import { transitions, positions } from "react-alert";
 
 export const options = {
   position: positions.BOTTOM_RIGHT,
-  timeout: 4000,
+  timeout: 3000,
   offset: "30px",
   transition: transitions.SCALE,
 };
